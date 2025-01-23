b295eb528dafd68e3d808e483181228b553ddcfd
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2025-01-23 10:07:37 +0700

    feat(cors): Enable cors for API

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 dotenvExpand.expand(dotenv.config());
@@ -16,6 +17,7 @@ const init = async () => {
     const port = process.env.API_PORT ? +process.env.API_PORT : 5000;
     await seedService.seed();
     const app = express();
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use('/public', express.static('uploads'))
