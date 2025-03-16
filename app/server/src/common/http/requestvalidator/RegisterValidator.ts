284f9940ccb973af241df9743ec17dff1508d402
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2025-03-16 14:01:30 +0700

    ref(Register): Make institutionId opsional

--- app/server/src/common/http/requestvalidator/RegisterValidator.ts
+++ app/server/src/common/http/requestvalidator/RegisterValidator.ts
@@ -6,11 +6,7 @@ export const registerPayloadSchema = joi.object({
   password: joi.string().min(6).required(),
   role: joi.number().min(2).max(6),
   isVerified: joi.boolean(),
-  institutionId: joi.when("role", {
-    is: 6,
-    then: joi.number().required(),
-    otherwise: joi.number(),
-  }),
+  institutionId: joi.number(),
 });
 
 export const institutionRegisterPayloadSchema = joi.object({
